893d010baf1df0bdf8393714f255de660722a730
¨Andy¨ <andy@scalevp.com>
2011-09-23 14:48:32 -0700

    Create portfolio now working

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,8 +1,8 @@
 class Position < ActiveRecord::Base
 	has_one :asset
 	
-	accepts_nested_attributes_for :asset
+	# accepts_nested_attributes_for :asset
 	
-	attr_accessible :asset_id, :holding_percentage, :asset_attributes
+	attr_accessible :asset_id, :holding_percentage #, :asset_attributes
 	
 end
