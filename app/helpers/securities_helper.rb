c308ac4d2a723cae6c04ae8be5f08b5f4bc03ef4
¨Andy¨ <andy@scalevp.com>
2011-10-26 15:17:31 -0700

    Reduce unnecessary Yahoo requests

--- app/helpers/securities_helper.rb
+++ app/helpers/securities_helper.rb
@@ -6,15 +6,21 @@ module SecuritiesHelper
   # ----------------------------------------------------------------------------------------------------------   
   def tickers_to_names(tickers)
     
-    # Request a StandardQuote to get the company names
-    quote_type = YahooFinance::StandardQuote
-    quotes     = YahooFinance::get_quotes( quote_type, tickers )
-
-    # Check that tickers exist
-
-    # create array of names
     name_array = Array.new
-    tickers.each { |x| name_array << quotes[x.upcase].name.titleize }
+    
+    tickers.each do |t|
+      sec = Security.find_by_ticker(t)
+      if sec
+        name_array << sec.name
+      else
+        # Request a StandardQuote to get the company names
+        quote_type = YahooFinance::StandardQuote
+        quote      = YahooFinance::get_quotes( quote_type, t )
+        name_array << quote[t.upcase].name.titleize
+      end
+    end
+    
     return name_array
+
   end     
 end
