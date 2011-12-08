85c54a0b369c1382aa41a561b64b7d0a3fb0c20f
¨Andy¨ <andy@scalevp.com>
2011-12-07 16:07:51 -0800

    Cache topsy feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    # @users = User.all
-    @news  = Topsy.search("asset correlation", :perpage => 3, :page => 1, :window => 'm')
+    @news = Rails.cache.fetch('asset-correlation', :expires_in => 24.hours) do 
+      Topsy.search("asset correlation", :perpage => 3, :page => 1, :window => :auto)
+    end
   end
 end
