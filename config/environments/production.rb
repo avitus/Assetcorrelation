c32c151494662cffcea1cc0f3e7fc6e54ce09101
Andy <avitus@gmail.com>
2013-05-23 15:48:03 -0700

    Clean up log file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,32 +1,48 @@
 Assetcorrelation::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # Code is not reloaded between requests
-  config.cache_classes = true
-
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  #===============================
+  # Caching
+  #===============================
+  config.cache_classes = true                      # Code is not reloaded between requests
+  config.consider_all_requests_local = false       # Full error reports are disabled
+  config.cache_store = :dalli_store                # Use Memcached for cache
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+  #===============================
+  # Asset Pipeline
+  #===============================
+  config.assets.compress = true               # Compress Javascript and CSS
+  config.assets.js_compressor  = :uglifier    # Javascript compression
+  config.assets.digest = true                 # Generate digests for assets URLs
+  config.serve_static_assets = false          # Disable Rails's static asset server (Apache or nginx will already do this)
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true # This should be false but seems to be a problem with Rails Admin ... try again with Rails 3.1.3
 
-  # Generate digests for assets URLs
-  config.assets.digest = true
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(rails_admin/rails_admin.js rails_admin/rails_admin.css) # This is a temporary workaround until Rails 3.1.1 Should be able to remove
 
+  # Ensure that Ckeditor assets are precompiled
+  config.assets.precompile += Ckeditor.assets
+
+  # Defaults to Rails.root.join("public/assets")
+  # config.assets.manifest = YOUR_PATH
 
   #===============================
   # Content Delivery Network
   #===============================
-  config.action_controller.asset_host      = "http://d2fi0e71yegorg.cloudfront.net"  # Amazon Cloudfront
-  config.static_cache_control              = "public, max-age=86400"
+  config.action_controller.asset_host = "http://d2fi0e71yegorg.cloudfront.net"  # Amazon Cloudfront
+  config.static_cache_control = "public, max-age=86400"
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # Header that Nginx uses for sending files
+
+  #===============================
+  # Logging
+  #===============================
+  config.logger = Logger.new(config.paths['log'].first, 5, 100.megabytes)  # Let Rails handle log rotation
+  config.log_level = :info
+  config.active_support.deprecation = :notify     # Send deprecation notices to registered listeners
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -37,15 +53,9 @@ Assetcorrelation::Application.configure do
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
 
-  # Use a different cache store in production
-  config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w(rails_admin/rails_admin.js rails_admin/rails_admin.css) # This is a temporary workaround until Rails 3.1.1 Should be able to remove
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -57,8 +67,6 @@ Assetcorrelation::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'assetcorrelation.com' }
   # ActionMailer Config
